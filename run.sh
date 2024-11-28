81c3f39e26fbb22304f2c8e68bb40e60640eaba8
Julius Heitkoetter <juliush@mit.edu>
2024-11-28 18:25:18 -0500

    adding logging

--- run.sh
+++ run.sh
@@ -1,5 +1,9 @@
 # Scripts to run to produce plots for assignment
 
+if [ ! -d "logs" ]; then
+  mkdir logs
+fi
+
 # Part 1:
 # - stats_assignment.py with n_pseudo=200 and mass range 105-155
 # - stats_assignment.py with n_pseudo=200 and mass range 120-135
@@ -7,10 +11,10 @@
 
 # Part 2:
 
-python cls.py --n_pseudoexperiments_per_mass_bin 200 --dataset data/toy_dataset_smaller_peak.csv.npy --N_sig_expected 22  --plot_saving_folder "figures/cls_small_nsig22"
-python cls.py --n_pseudoexperiments_per_mass_bin 200 --dataset data/toy_dataset_smaller_peak.csv.npy --N_sig_expected 45  --plot_saving_folder "figures/cls_small_nsig45"
+python cls.py --n_pseudoexperiments_per_mass_bin 200 --dataset data/toy_dataset_smaller_peak.csv.npy --N_sig_expected 22  --plot_saving_folder "figures/cls_small_nsig22" > logs/cls_small_nsig22 2>&1
+python cls.py --n_pseudoexperiments_per_mass_bin 200 --dataset data/toy_dataset_smaller_peak.csv.npy --N_sig_expected 45  --plot_saving_folder "figures/cls_small_nsig45" > logs/cls_small_nsig45 2>&1
 
-python cls_enhanced.py --n_pseudoexperiments 400 --dataset data/toy_dataset_smaller_peak.csv.npy --N_sig_expected 22  --plot_saving_folder "figures/clsEnhanced_small_nsig22"
-python cls_enhanced.py --n_pseudoexperiments 400 --dataset data/toy_dataset_smaller_peak.csv.npy --N_sig_expected 45  --plot_saving_folder "figures/clsEnhanced_small_nsig45"
-python cls_enhanced.py --n_pseudoexperiments 400 --dataset data/toy_dataset.csv.npy --N_sig_expected 45  --plot_saving_folder "figures/clsEnhanced_large_nsig45"
+python cls_enhanced.py --n_pseudoexperiments 400 --dataset data/toy_dataset_smaller_peak.csv.npy --N_sig_expected 22  --plot_saving_folder "figures/clsEnhanced_small_nsig22" > logs/clsEnhanced_small_nsig22 2>&1
+python cls_enhanced.py --n_pseudoexperiments 400 --dataset data/toy_dataset_smaller_peak.csv.npy --N_sig_expected 45  --plot_saving_folder "figures/clsEnhanced_small_nsig45" > logs/clsEnhanced_small_nsig45 2>&1
+python cls_enhanced.py --n_pseudoexperiments 400 --dataset data/toy_dataset.csv.npy --N_sig_expected 45  --plot_saving_folder "figures/clsEnhanced_large_nsig45" > logs/clsEnhanced_large_nsig45 2>&1
 
